382dcf6e314eee79f6f6224dea29bcfe43dd326b
leetaifook <lee@leetaifook.com>
2022-01-09 08:11:39 +0800

    update depn

--- init/manage/init.go
+++ init/manage/init.go
@@ -17,23 +17,23 @@ import (
 )
 
 func init() {
-	baseConfFile := flag.String("config.plugins.webrouter_plugin_template.manage.base", "../config/plugins/webrouter_plugin_template/base.conf", "base config file")
-	var baseConf base.Config
-	config.Files(*baseConfFile).Load(&baseConf)
-
-	baseCache := cache.New(baseConf.CacheDefaultExpiration.Duration, baseConf.CacheCleanupInterval.Duration)
-	baseDB := mongodb.NewMongoPool("", baseConf.MongoDatabaseName, 100, options.Client().ApplyURI(baseConf.MongoDNS))
-	webrouter.SetCloser(func() { baseDB.Disconnect(context.TODO()) })
-
-	baseDB.M().InitCollection(schema.Collection1)
-
-	baseService := base.NewService(&baseConf, baseCache, baseDB)
-
 	manageConfFile := flag.String("config.webrouter_plugin_template.manage", "../config/plugins/webrouter_plugin_template/manage.conf", "manage config file")
 	var manageConf manage.Config
 	config.Files(*manageConfFile).Load(&manageConf)
 
 	if manageConf.WebRouter {
+		baseConfFile := flag.String("config.plugins.webrouter_plugin_template.manage.base", "../config/plugins/webrouter_plugin_template/base.conf", "base config file")
+		var baseConf base.Config
+		config.Files(*baseConfFile).Load(&baseConf)
+
+		baseCache := cache.New(baseConf.CacheDefaultExpiration.Duration, baseConf.CacheCleanupInterval.Duration)
+		baseDB := mongodb.NewMongoPool("", baseConf.MongoDatabaseName, 100, options.Client().ApplyURI(baseConf.MongoDNS))
+		webrouter.SetCloser(func() { baseDB.Disconnect(context.TODO()) })
+
+		baseDB.M().InitCollection(schema.Collection1)
+
+		baseService := base.NewService(&baseConf, baseCache, baseDB)
+
 		manageConf.Config = baseConf
 		manageService := manage.NewService(&manageConf, baseService)
 
--- init/serve/init.go
+++ init/serve/init.go
@@ -17,23 +17,23 @@ import (
 )
 
 func init() {
-	baseConfFile := flag.String("config.plugins.webrouter_plugin_template.serve.base", "../config/plugins/webrouter_plugin_template/base.conf", "base config file")
-	var baseConf base.Config
-	config.Files(*baseConfFile).Load(&baseConf)
-
-	baseCache := cache.New(baseConf.CacheDefaultExpiration.Duration, baseConf.CacheCleanupInterval.Duration)
-	baseDB := mongodb.NewMongoPool("", baseConf.MongoDatabaseName, 100, options.Client().ApplyURI(baseConf.MongoDNS))
-	webrouter.SetCloser(func() { baseDB.Disconnect(context.TODO()) })
-
-	baseDB.M().InitCollection(schema.Collection1)
-
-	baseService := base.NewService(&baseConf, baseCache, baseDB)
-
 	serveConfFile := flag.String("config.webrouter_plugin_template.serve", "../config/plugins/webrouter_plugin_template/serve.conf", "serve config file")
 	var serveConf serve.Config
 	config.Files(*serveConfFile).Load(&serveConf)
 
 	if serveConf.WebRouter {
+		baseConfFile := flag.String("config.plugins.webrouter_plugin_template.serve.base", "../config/plugins/webrouter_plugin_template/base.conf", "base config file")
+		var baseConf base.Config
+		config.Files(*baseConfFile).Load(&baseConf)
+
+		baseCache := cache.New(baseConf.CacheDefaultExpiration.Duration, baseConf.CacheCleanupInterval.Duration)
+		baseDB := mongodb.NewMongoPool("", baseConf.MongoDatabaseName, 100, options.Client().ApplyURI(baseConf.MongoDNS))
+		webrouter.SetCloser(func() { baseDB.Disconnect(context.TODO()) })
+
+		baseDB.M().InitCollection(schema.Collection1)
+
+		baseService := base.NewService(&baseConf, baseCache, baseDB)
+
 		serveConf.Config = baseConf
 		serveService := serve.NewService(&serveConf, baseService)
 
